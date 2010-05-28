bc38e3c3588a73fe75cb3b392de1789a3c8adddc
Austin Bales <arbales@gmail.com>
2010-05-28 17:14:31 -0400

    Added Readme

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+gem "sinatra"
+gem "haml"
+gem "sinatra_more"  
+gem "shotgun"
+gem "pony"
--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
-require 'rubygems'
-require 'sinatra'
-require 'sinatra_more'
+require "rubygems"
+require "bundler"
+Bundler.setup
 
-get '/' do
-  erb :index
+require 'sinatra'
+require 'sinatra_more/markup_plugin'
+require 'sinatra_more/render_plugin'
+require 'haml'
+require 'pony'
+   
+helpers do
+  register SinatraMore::MarkupPlugin
+  register SinatraMore::RenderPlugin
 end
+
+load 'routes.rb'
